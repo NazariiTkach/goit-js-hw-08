29fa996ceb4851d8bb848375f775c2929c8aa1b7
Nazarii Tkach <113298067+NazariiTkach@users.noreply.github.com>
2023-02-14 20:54:27 +0000

    commit-2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,66 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryContainer = document.querySelector(".gallery");
+
+const imageMarkup = createImagesGalleryMarkup(galleryItems);
+
+galleryContainer.innerHTML = imageMarkup;
+
+// galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function createImagesGalleryMarkup(gallery) {
+ return gallery
+    .map(({ preview, original, description }) => {
+      return ` <div class="gallery__item">
+       <a class="gallery__link" href="${original}">
+          <img class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}" />
+        </a>
+    </div> `;
+    }).join('');
+
+};
+
+// console.log(createImagesGalleryMarkup(galleryItems));
+
+// function onGalleryContainerClick(evt) {
+// 	evt.preventDefault();
+// 	if (!evt.target.classList.contains("gallery__image")) {
+
+// 		return
+// 	}
+
+// 	const instance = basicLightbox.create(`
+// <img src="${evt.target.dataset.source}" width="800" height="600">`, 
+//   {
+//   onShow: () => {
+//    document.addEventListener('keydown', handelKeydown)
+//   },
+//   onClose: () => {
+//    document.removeEventListener('keydown', handelKeydown)
+//     }
+//   }
+//   )
+  
+//   instance.show()
+
+
+//   function handelKeydown(evt) {
+//     if (evt.key === 'Escape') {
+// 			instance.close()
+// 		}
+//   }
+		
+// }
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
